486e222b06b512cedba04d4c47379897978de8de
TheDeafOne <woodburnkb20@gcc.edu>
2022-10-09 11:09:27 -0400

    initial commit

---
+++ algorithms/csps/ac3.py
@@ -0,0 +1,10 @@
+'''
+    An implementation of Mackworth's Arc Consistency Algorithm Three (AC3)
+'''
+
+class AC3:
+    def __init__(self):
+        self.value = 'ac3 test'
+    
+    def run(self):
+        print(self.value)
