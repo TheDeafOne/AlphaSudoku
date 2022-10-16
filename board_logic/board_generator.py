3512bf93b940efc1e4bcaa740d69f5717cf1f8f2
TheDeafOne <woodburnkb20@gcc.edu>
2022-10-16 17:36:48 -0400

    moved large file parsing to separate folder

--- board_logic/board_generator.py
+++ board_logic/board_generator.py
@@ -54,36 +54,3 @@ class BoardGenerator:
         dataset.seek(offset*self._row_char_count)
         dataset.readline()
         return dataset.readline().split(',')
-    
-    # Don't delete this, it's useful for parsing new datasets
-    # def rewrite(self):
-    #     file = open('./data/initial_boards/sudoku_dataset.csv','w', newline='')
-    #     writer = csv.writer(file)
-    #     f = open('./data/initial_boards/sudoku.csv')
-    #     reader = csv.reader(f)
-        
-    #     vals = {}
-    #     for line in reader:
-    #         if len(line) > 0:
-    #             cnt = line[0].count('0')
-    #             if cnt in vals:
-    #                 vals[cnt].append(line)
-    #             else:
-    #                 vals[cnt] = [line]
-
-    #     for val in sorted(vals.keys()):
-    #         for row in vals[val]:
-    #             writer.writerow(row)
-
-    # Do not delete,  it's useful for debugging/reading the file without crashing things
-    # def read_sudoku_set(self):
-    #     f = open('./data/initial_boards/sudoku_dataset.csv')
-    #     reader = csv.reader(f)
-    #     cnt = 0
-    #     for row in reader:
-    #         if cnt % 90000 == 0:
-    #             print(len(''.join(row)))
-    #             row = row[0]
-    #             print(row, row.count('0'))
-                
-    #         cnt += 1
