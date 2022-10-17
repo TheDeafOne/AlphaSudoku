e233998b3c4280bcec62478fbc73ccdf14690b3a
TheDeafOne <woodburnkb20@gcc.edu>
2022-10-16 21:08:45 -0400

    scratch testing

--- testing/testing_board_indexing.py
+++ testing/testing_board_indexing.py
@@ -1,22 +1,11 @@
 import sys, os
 sys.path.append(os.path.join(sys.path[0], "../"))
 
-from board_logic.board_indexing import BoardIndexing
 from board_logic.board import Board
 from board_logic.board_generator import BoardGenerator
 
-board_indexes = BoardIndexing()
 board = Board()
 generator = BoardGenerator()
-
-def display_cells():
-    print(board_indexes.get_cell_indexes())
-
-def display_cell_units():
-    print(board_indexes.get_unit_indexes())
-
-def display_cell_peers():
-    print(board_indexes.get_peer_indexes())
 
 def try_set_board():
     did_set = board.set_board('001')
@@ -27,9 +16,14 @@ def try_set_board():
 def generate_board():
     print(generator.generate_board())
 
-    
+def test_board_grouping():
+    nb = generator.generate_board()
+    board.set_board(nb)
+    td_board = board.get_board_2D()
+    [print(row) for row in td_board]
+    print(board.get_group_board())
 
 if __name__ == '__main__':
-    
+    test_board_grouping()
 
     pass
